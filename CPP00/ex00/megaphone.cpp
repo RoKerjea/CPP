0afd021c5b4e54f8ffb62b504c3acb87d5c2e258
RoKerjea <rokerjea@student.42.fr>
2023-01-22 18:19:14 +0100

    multi inputs should work

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,20 +6,30 @@
 /*   By: rokerjea <rokerjea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 18:06:56 by rokerjea          #+#    #+#             */
-/*   Updated: 2023/01/20 14:06:13 by rokerjea         ###   ########.fr       */
+/*   Updated: 2023/01/22 18:18:58 by rokerjea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 
-int	main(int ac, char **s)
+std::string	get_scream(int ac, char **av)
 {
 	std::string	scream;
 	if (ac == 1)
 		scream = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
-		scream = s[1]; //need to add every string if ac > 2
+	{
+		for (int i = 1; i < ac; i++)
+			scream += av[i];
+	}
+	return (scream);
+}
+
+int	main(int ac, char **s)
+{
+	std::string	scream;
+	scream = get_scream(ac, s);
 	for (int i = scream.length(); i >= 0; i--)
 	{
 		scream[i] = std::toupper(scream[i]);
