4f75a5d1b3cf446e2f88aed494931408bf9aba0b
RoKerjea <rokerjea@student.42.fr>
2023-05-27 21:37:15 +0200

    bleur

--- CPP09/ex01/main.cpp
+++ CPP09/ex01/main.cpp
@@ -1,3 +1,15 @@
 /*stack/deque to use because of specificity of subject?
 RPN is conceived with stack as a tool, push and pop by order of reading
 so stack seems a logical choice*/
+#include "RPN.hpp"
+
+int	main(int ac, char **av) {
+	try {
+		checkArgumentErrors(ac, av[1], 0);
+		rpn(av[1], 0);
+	}
+	catch(const std::exception& e) {
+		std::cerr << REDCOLOR << e.what() << ENDCOLOR << std::endl;
+	}
+	return 0;
+}
